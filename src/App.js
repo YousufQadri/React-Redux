a3208bfa4124cee4590a6b1b067a41d77e97fcaa
Yousuf <s.yousuf@live.com>
2019-08-20 13:13:34 +0500

    Working with local state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    counterVal: 10
+  };
+
+  onUp = () => {
+    this.setState({
+      counterVal: this.state.counterVal + 1
+    });
+  };
+  onDown = () => {
+    this.setState({
+      counterVal: this.state.counterVal - 1
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Counter</h1>
+        <h3>{this.state.counterVal}</h3>
+        <button onClick={this.onUp}>Up</button>
+        <button onClick={this.onDown}>Down</button>
+      </div>
+    );
+  }
 }
 
 export default App;
