08b72d28e2350a9d79ed6f1dbe96c307c2553bf4
Yousuf <s.yousuf@live.com>
2019-08-20 14:01:29 +0500

    Simple redux implemented

--- src/App.js
+++ src/App.js
@@ -1,32 +1,49 @@
 import React from "react";
 import "./App.css";
+import { connect } from "react-redux";
 
 class App extends React.Component {
-  state = {
-    counterVal: 10
-  };
+  // state = {
+  //   counterVal: 10
+  // };
 
-  onUp = () => {
-    this.setState({
-      counterVal: this.state.counterVal + 1
-    });
-  };
-  onDown = () => {
-    this.setState({
-      counterVal: this.state.counterVal - 1
-    });
-  };
+  // onUp = () => {
+  //   this.setState({
+  //     counterVal: this.state.counterVal + 1
+  //   });
+  // };
+  // onDown = () => {
+  //   this.setState({
+  //     counterVal: this.state.counterVal - 1
+  //   });
+  // };
 
   render() {
     return (
       <div className="App">
         <h1>Counter</h1>
-        <h3>{this.state.counterVal}</h3>
-        <button onClick={this.onUp}>Up</button>
-        <button onClick={this.onDown}>Down</button>
+        <h3>{this.props.counterVal}</h3>
+        <button onClick={this.props.onUp}>Up</button>
+        <button onClick={this.props.onDown}>Down</button>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    counterVal: state.counterVal
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onUp: () => dispatch({ type: "ON_UP" }),
+    onDown: () => dispatch({ type: "ON_DOWN" })
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
--- src/Store/Reducer/index.js
+++ src/Store/Reducer/index.js
@@ -2,6 +2,19 @@ const initialState = {
   counterVal: 10
 };
 
-const reducer = (state = initialState, action) => {};
+const reducer = (state = initialState, action) => {
+  const newState = { ...state };
+
+  switch (action.type) {
+    case "ON_UP":
+      newState.counterVal++;
+      break;
+
+    case "ON_DOWN":
+      newState.counterVal--;
+      break;
+  }
+  return newState;
+};
 
 export default reducer;
